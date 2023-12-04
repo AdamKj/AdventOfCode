f6786141f835fa68dc366d27abac23c2cf1de69a
Adam Kjellberg <kjellberg@smoothcomp.com>
2023-12-04 11:13:51 +0100

    Changed day1-part1 code

--- 2023/day-1/part1.js
+++ 2023/day-1/part1.js
@@ -1,11 +1,29 @@
-document.querySelector("pre").split("\n").map(word => {
-    word.split('').map(char => {
-        Object.keys(obj).map(key => {
-            if (key[0] === char) {
+const obj = {
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9',
+    'zero': '0'
+}
 
+document.querySelector("pre").innerText.split("\n").map(word => {
+    let a = word
+        .split('')
+        .map(char => {
+            for (const key in obj) {
+                if (key[0] === char) {
+                    return obj[key];
+                }
             }
+            return '';
         })
-    })
+        .filter(val => val !== '');
+
     if (a?.length > 0) {
         if (a.length > 1) {
             return Number(a[0] + a[a.length - 1])
